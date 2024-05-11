b75217e319ee4e69e341113e9154e07cd6bd64ac
camilogonzalez7424 <gonzalezcamilo508@gmail.com>
2024-05-10 23:12:10 -0500

    added process vote code

---
+++ process-vote/handler.js
@@ -0,0 +1,31 @@
+var funker = require('funker');
+var pg = require('pg');
+
+function processVote(args) {
+  return new Promise((resolve, reject) => {
+    var client = new pg.Client('postgres://postgres@db/postgres');
+    client.connect();
+    client.query("CREATE TABLE IF NOT EXISTS votes (id SERIAL PRIMARY KEY, vote VARCHAR(255) NOT NULL)")
+      .then(() => {
+        return client.query("INSERT INTO votes (vote) VALUES ($1)", [args.vote]);
+      })
+      .then(() => {
+        client.end();
+        resolve();
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+funker.handle(function(args, callback) {
+  // Return immediately so this function runs in the background
+  callback();
+
+  console.log("Processing vote for", args.vote);
+
+  processVote(args)
+    .then(() => console.log("Vote processed successfully"))
+    .catch(err => console.error("Error processing vote:", err));
+});
